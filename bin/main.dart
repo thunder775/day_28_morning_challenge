e9562a84428a1bdca6e7ab4d3e0a4f13ea165455
McLarenCollege <hello@mclarencollege.in>
2019-11-11 08:30:33 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+// Challenge
+// Flatten a list
+// Create a function that takes a list. This list can have all kinds of items, even other lists. The function should return a single, flat, one-dimensional, list with all elements. Here are the conditions:
+//
+// - If the item is a list, include each item in it and the following still apply:
+// - If the item is a Function, include the function's output, not the function itself.
+// - If the item is a plain Object or a Primitive, include it as is.
+
+// Example
+// flattenList([1, "2", [3, function () { return 4; }, [ "five" ], "six", true, { prop: "val" }]])
+//  ➞ [1, "2", 3, 4, "five", "six", true, { prop: "val" }]
+
+main() {
+}
---
+++ test/Day_28_Morning_Challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:Day_28_Morning_Challenge/Day_28_Morning_Challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
